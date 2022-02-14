d7711d1cd49768263926235c917c0a96e18caa29
R87P <35150169+Rparra8787@users.noreply.github.com>
2022-02-14 11:12:47 -0700

    commit on master

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
- 
+let addToDoButton = document.getElementById('add-todo');
+let toDoContainer = document.getElementById('todo-container');
+let inputField = document.getElementById('input-field');
+
+
+addToDoButton.addEventListener('click', function() {
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = ' ';
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
